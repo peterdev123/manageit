8f0f531c56833bb2d48e87910528a2b4fe45f88a
peterdev123 <petersylvanv123@gmail.com>
2025-05-27 12:14:35 +0800

    Trigger redeploy

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN?.split(','),
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
